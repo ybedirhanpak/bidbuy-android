55b59d13c4982d8ba53248216e868c84a7e0932e
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2021-01-07 18:22:16 +0300

    Product.java: Add copy constructor, add constructor with only id

--- app/src/main/java/com/yabepa/bidbuy/data/Product.java
+++ app/src/main/java/com/yabepa/bidbuy/data/Product.java
@@ -5,9 +5,21 @@ public class Product {
     public String name;
     public String price;
 
+    public Product(String id) {
+        this.id = id;
+        this.name = "Product " + id;
+        this.price = "0";
+    }
+
     public Product(String id, String name, String price) {
         this.id = id;
         this.name = name;
         this.price = price;
+    }
+
+    public Product(Product product) {
+        this.id = product.id;
+        this.name = product.name;
+        this.price = product.price;
     }
 }
