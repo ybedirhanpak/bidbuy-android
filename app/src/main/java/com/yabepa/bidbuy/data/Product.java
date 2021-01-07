d8350fcf005dfa620834bcaf1c9b69872a809386
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2021-01-07 12:19:54 +0300

    Product.java: Add id and name to product class

--- app/src/main/java/com/yabepa/bidbuy/data/Product.java
+++ app/src/main/java/com/yabepa/bidbuy/data/Product.java
@@ -1,10 +1,13 @@
 package com.yabepa.bidbuy.data;
 
 public class Product {
-
+    public String id;
+    public String name;
     public String price;
 
-    public Product(String price) {
+    public Product(String id, String name, String price) {
+        this.id = id;
+        this.name = name;
         this.price = price;
     }
 }
