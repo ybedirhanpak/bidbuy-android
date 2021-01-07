2270239878345f89687f98eb50153cda7e7b0c1c
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2021-01-07 18:18:25 +0300

    Util.java: Add conversion functions between json string and input/output streams

---
+++ app/src/main/java/com/yabepa/bidbuy/common/Util.java
@@ -0,0 +1,16 @@
+package com.yabepa.bidbuy.common;
+
+import java.io.*;
+
+public class Util {
+
+    public static String inputStreamToJson(InputStream stream) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+        return reader.readLine();
+    }
+
+    public static void writeJsonToOutputStream(String json, OutputStream stream) throws IOException {
+        DataOutputStream requestStream = new DataOutputStream(stream);
+        requestStream.writeBytes(json + '\n');
+    }
+}
